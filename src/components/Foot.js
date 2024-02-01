f224a6b7bc7fe5c5c258b5534ae2d1c0c4ef30f1
aditya-jnu <29adityamishra@gmail.com>
2024-02-01 16:25:39 +0530

    few change

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,6 +1,6 @@
 function Foot(){
   function openLinkInNewTab(event){
-    if(event.target.innerHTML=="API")
+    if(event.target.innerHTML==="API")
      window.open("https://github.com/Hipo/university-domains-list-api", "_blank");
     else{
      window.open("https://github.com/aditya-jnu/React-Uni", "_blank");
